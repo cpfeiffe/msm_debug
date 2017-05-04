d0a074820d7caa6f0a787f3ae6d0e4aaa819e9f5
cpfeiffe <cpfeiffe@users.noreply.github.com>
2017-05-04 13:29:25 -0500

    More Changes

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -5,6 +5,7 @@ class DirectorsController < ApplicationController
 
   def show
     @director = Director.find(params[:id])
+    render("show")
   end
 
   def new_form
@@ -12,6 +13,7 @@ class DirectorsController < ApplicationController
 
   def create_row
     @director = Director.new
+
     @director.dob = params[:dob]
     @director.name = params[:name]
     @director.bio = params[:bio]
@@ -29,14 +31,16 @@ class DirectorsController < ApplicationController
   def update_row
     @director = Director.find(params[:id])
 
-    @director.dob = params[":dob"]
-    @director.name = params[":name"]
-    @director.bio = params[":bio"]
-    @director.image_url = params[":image_url"]
+    @director.dob = params[:the_dob]
+    @director.name = params[:the_name]
+    @director.bio = params[:the_bio]
+    @director.image_url = params[:the_image_url]
 
     @director.save
 
-    render("show")
+    render("directors/show.html.erb")
+
+
   end
 
   def destroy
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,12 +13,12 @@ class MoviesController < ApplicationController
   def create_row
     @movie = Movie.new
 
-    @movie.title = params[:the_title]
-    @movie.year = params[:the_year]
-    @movie.duration = params[:the_duration]
-    @movie.description = params[:the_description]
-    @movie.image_url = params[:the_image_url]
-    @movie.director_id = params[:the_director_id]
+    @movie.title = params[:title]
+    @movie.year = params[:year]
+    @movie.duration = params[:duration]
+    @movie.description = params[:description]
+    @movie.image_url = params[:image_url]
+    @movie.director_id = params[:director_id]
 
     @movie.save
 
@@ -32,12 +32,14 @@ class MoviesController < ApplicationController
   def update_row
     @movie = Movie.find(params[:id])
 
-    @movie.title = params[:the_title]
-    @movie.year = params[:the_year]
-    @movie.duration = params[:the_duration]
-    @movie.description = params[:the_description]
-    @movie.image_url = params[:the_image_url]
-    @movie.director_id = params[:the_director_id]
+    @movie.title = params[:title]
+    @movie.year = params[:year]
+    @movie.duration = params[:duration]
+    @movie.description = params[:description]
+    @movie.image_url = params[:image_url]
+    @movie.director_id = params[:director_id]
+
+    @movie.save
 
     render("show")
   end
